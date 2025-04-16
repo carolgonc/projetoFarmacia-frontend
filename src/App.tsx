5611a70ce50e44e083b38c2fcbf85451e857b30c
carolgonc <carolinypeixoto@outlook.com>
2025-04-16 17:46:22 -0300

    CRUD completo da Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import './App.css'
+import ListaCategorias from './components/categorias/listacategorias/ListaCategorias'
+import FormCategoria from './components/categorias/formcategoria/FormCategoria'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategorias" element={<FormCategoria />} />
+              <Route path="/editarcategorias/:id" element={<FormCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
+            			    bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Farmácia Bem-Estar
+                <Link to='/home' className="text-2xl font-bold">Farmácia Bem-Estar</Link>
 
                     <div className='flex gap-4'>
-                        Categorias
-                        Cadastrar categorias
+                    <Link to='/categorias' className='hover:underline'>Categorias</Link>
+                    <Link to='/cadastrarcategorias' className='hover:underline'>Cadastrar categorias</Link>
                     </div>
                 </div>
             </div>
