7924f0df089a7bec40c8b01521f9609526a5f368
carolgonc <carolinypeixoto@outlook.com>
2025-04-17 08:23:26 -0300

    atualizações

--- src/components/categorias/deletarcategoria/DeletarCategoria.tsx
+++ src/components/categorias/deletarcategoria/DeletarCategoria.tsx
@@ -43,22 +43,21 @@ function DeletarCategoria() {
             <h1 className='text-4xl text-center my-4'>Deletar categoria</h1>
             <p className='text-center font-semibold mb-4'>
                 Você tem certeza de que deseja apagar a categoria a seguir?</p>
-            <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
+            <div className='border-3 border-rose-950 flex flex-col rounded-4xl overflow-hidden justify-between shadow-md w-90 h-50'>
                 <header 
-                    className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>
-                    Categoria
+                    className='py-2 px-6   border-rose-950 font-bold text-1xl text-center'>
+                    {categoria.nome}
                 </header>
-                <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
-                <div className="flex">
+                <p className='p-2 text-xl  h-full text-center'>{categoria.descricao}</p>
+                <div className="flex justify-between px-4 py-4 gap-4">
                     <button 
-                        className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2'
+                        className='w-6/10 py-2 bg-red-500 hover:bg-red-700 text-white font-medium rounded-xl shadow-md transition duration-300'
                         onClick={retornar}>
                         Não
                     </button>
                     <button 
-                        className='w-full text-slate-100 bg-indigo-400 
-                                   hover:bg-indigo-600 flex items-center justify-center'
-                                   onClick={deletarCategoria}>
+                        className='w-6/10 py-2 bg-indigo-500 hover:bg-indigo-700 text-white font-medium rounded-xl shadow-md transition duration-300'
+                                    onClick={deletarCategoria}>
                         {isLoading ?
                             <RotatingLines
                                 strokeColor="white"
